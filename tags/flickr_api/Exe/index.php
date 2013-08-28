8281e68efc3419833fc397e53e4fe9e1b45b9228
mas1485@gmail.com <mas1485@gmail.com@79bd5456-f6ba-11de-885d-7376d8f76b83>
2013-08-28 12:26:43 +0000

    === Don't know what is inside. Just committing to save changes ===

--- tags/flickr_api/Exe/index.php
+++ tags/flickr_api/Exe/index.php
@@ -6,7 +6,12 @@ if( isset($_GET['error']) )
 require_once ('base.flickr.php');
 $controller= new Flickr('/Flickr_API/Exe/');
 
-$controller->delete_FDBI38992SDKFOGF();
+//$controller->delete_FDBI38992SDKFOGF();
+
+
+include_once "view.lat-long.php";
+$viewObject = new ShowLatLong('/Flickr_API/Exe/');
+$viewObject->show_ASDLGKMDFIWOERERW();
 
 }catch (Exception $e) {
 	echo "<h1>Caught exception: <font style='color:red'>{$e->getMessage()}</font></h1>";
