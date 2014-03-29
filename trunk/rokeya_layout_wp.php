2568e6b1a165610c8bb5d271b4a8119ca08bc0af
mas1485@gmail.com <mas1485@gmail.com@79bd5456-f6ba-11de-885d-7376d8f76b83>
2014-03-29 16:18:22 +0000

    Bugfix in wordpress plugin

--- trunk/rokeya_layout_wp.php
+++ trunk/rokeya_layout_wp.php
@@ -9,12 +9,15 @@ Version: 2.0
 
 function ADD_BN_BUTTON() {
 ?>
-<script type="text/javascript" src="<?PHP echo plugins_url( 'rokeya_layout-dev.js' , __FILE__ ); ?>"></script>
+<script type="text/javascript" src="<?PHP 
+//echo plugins_url( 'rokeya_layout-dev.js' , __FILE__ ); 
+echo "http://bangla-keyboard-layout.googlecode.com/svn/trunk/rokeya_layout-4.4.73.js";
+?>"></script>
 
 <script type="text/javascript">
 var handlerKeyboardPageOpener=function() {
 
-var sourceDiv = "<span style='margin-left:5px;' id='rokeya_button_bn'><a onclick=\"window.open('<?PHP echo plugins_url( 'index.html' , __FILE__ ); ?>', 'Rokeya', 'height=400,width=700,top=200,left=150,scrollbars=1');return false;\" class='button'>?.A</a></span>";
+var sourceDiv = "<span style='margin-left:5px;' id='rokeya_button_bn'><a onclick=\"window.open('<?PHP echo plugins_url( 'index.html' , __FILE__ ); ?>', 'Rokeya', 'height=400,width=700,top=200,left=150,scrollbars=1');return false;\" class='button'>অ.A</a></span>";
 if( jQuery("#view-post-btn").length ==1 )
 	jQuery("#view-post-btn").after(sourceDiv);
 else
@@ -28,14 +31,16 @@ jQuery("#pass2, #pass1").attr("noBanglaText","noBanglaText");
 if( window.location.href.indexOf("wp-admin/post-new.php") !== -1 || window.location.href.indexOf("wp-admin/profile.php") !== -1 )
 {
 	(function($){
-		var $textbox= jQuery("input:text, textarea").not('[noBanglaText]');
-		$textbox.each(function(index,valueOfElement){
-			if(this.id == '')
-			{
-				this.id= Math.random();
-			}
-			new banglaLayout(this.id).loadHelpTooltip();
-		});	
+		try{
+			var $textbox= jQuery("input:text, textarea").not('[noBanglaText]');
+			$textbox.each(function(index,valueOfElement){
+				if(this.id == '')
+				{
+					this.id= Math.random();
+				}
+				new banglaLayout(this.id).loadHelpTooltip();
+			});	
+		}catch(e){}
 	})(jQuery);
 }
 //*/
@@ -53,7 +58,7 @@ jQuery(handlerKeyboardPageOpener);
 
 	// this = collection of textarea or inputbox
 	this.each(function( index ) {
-		var toolTipText = "ctrl+m ???? F9 ???? ????? ? ???????? ???? ???? ??????..\n..?=H, ?=Z, ?=x, ?=X, ?=V, ?=B, ?=M";
+		var toolTipText = "ctrl+m অথবা F9 চেপে বাংলা ও ইংরেজীতে সুইচ করতে পারবেন..\n..হ=H, ৎ=Z, ঙ=x, ঞ=X, ং=V, ঁ=B, ঃ=M";
 		var $comment=$(this);
 		var str="<div class='tooltipOnRokeyaLayout' style='width: 10px; position:relative; cursor:help; color:red;";
 		str +=	"left:"+($comment.width())+"px; top:17px'><abbr title='"+toolTipText+"'>?</abbr></div>";
