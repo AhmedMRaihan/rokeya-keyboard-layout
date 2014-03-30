78fb9dcffcd175025adf7aa887486d3c08e1cea0
mas1485@gmail.com <seoulgithub@users.noreply.github.com>
2014-03-30 08:41:57 +0000

    Better try catch support

--- rokeya_layout-4.4.73.js
+++ rokeya_layout-4.4.73.js
@@ -28,32 +28,39 @@ function banglaLayout(id)
 			if(!!keyEvent)
 			 keyEvent.preventDefault();
 		}
-	}catch(e){ console.log(e.message); }
-	
+	}catch(e){ 
+		if(console) 
+			console.log(e.message); 
+	}
 	this.sourceField = id;
 	return this;
 }
 banglaLayout.prototype.loadHelpTooltip = function(){
-	if(!jQuery)
-		return this;
+	try{
+		if(!jQuery)
+			return this;
 
-	var $sourceField=document.getElementById(this.sourceField);
-	var toolTipText = "ctrl+m অথবা F9 চেপে বাংলা ও ইংরেজীতে সুইচ করতে পারবেন..\n..হ=H, ৎ=Z, ঙ=x, ঞ=X, ং=V, ঁ=B, ঃ=M";
-	
-	var $parent = jQuery("#"+this.sourceField);
-	var left = $parent.position().left+$parent.width();
-	left -= $parent.width() < 3 ? 0:3;
-	var top = $parent.position().top;
-	top += $parent.height() < 5 ? 0:5;
+		var $sourceField=document.getElementById(this.sourceField);
+		var toolTipText = "ctrl+m অথবা F9 চেপে বাংলা ও ইংরেজীতে সুইচ করতে পারবেন..\n..হ=H, ৎ=Z, ঙ=x, ঞ=X, ং=V, ঁ=B, ঃ=M";
+		
+		var $parent = jQuery("#"+this.sourceField);
+		var left = $parent.position().left+$parent.width();
+		left -= $parent.width() < 3 ? 0:3;
+		var top = $parent.position().top;
+		top += $parent.height() < 5 ? 0:5;
 
-	var $tooltipDiv = document.createElement('abbr');
-	$tooltipDiv.setAttribute("style","width: 10px; position:absolute; cursor:help; color:red; left:"+(left)+"px;top:"+(top)+"px;");
-	$tooltipDiv.innerHTML = "?";
-	$tooltipDiv.setAttribute("title", toolTipText);
-	$tooltipDiv.onmouseover=function(){};
-	
-	jQuery($tooltipDiv).insertAfter($parent);
+		var $tooltipDiv = document.createElement('abbr');
+		$tooltipDiv.setAttribute("style","width: 10px; position:absolute; cursor:help; color:red; left:"+(left)+"px;top:"+(top)+"px;");
+		$tooltipDiv.innerHTML = "?";
+		$tooltipDiv.setAttribute("title", toolTipText);
+		$tooltipDiv.onmouseover=function(){};
+		
+		jQuery($tooltipDiv).insertAfter($parent);
 	return this;
+	}catch(e){ 
+		if(console) 
+			console.log(e.message); 
+	}
 }
 
 
