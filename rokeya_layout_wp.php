e7fd341d5483b84b3934f417c21f5de4ade18c37
mas1485@gmail.com <seoulgithub@users.noreply.github.com>
2014-04-02 22:35:40 +0000

    Wordpress plugin worked in localhost.

--- rokeya_layout_wp.php
+++ rokeya_layout_wp.php
@@ -9,50 +9,50 @@ Version: 2.0
 
 function ADD_BN_BUTTON() {
 ?>
+
 <script type="text/javascript" src="<?PHP 
 //echo plugins_url( 'rokeya_layout-dev.js' , __FILE__ ); 
 echo "http://bangla-keyboard-layout.googlecode.com/svn/trunk/rokeya_layout-4.4.73.js";
 ?>"></script>
 
 <script type="text/javascript">
-var handlerKeyboardPageOpener=function() {
-
-var sourceDiv = "<span style='margin-left:5px;' id='rokeya_button_bn'><a onclick=\"window.open('<?PHP echo plugins_url( 'index.html' , __FILE__ ); ?>', 'Rokeya', 'height=400,width=700,top=200,left=150,scrollbars=1');return false;\" class='button button-small'>অ.A</a></span>";
-if( jQuery("#view-post-btn").length ==1 )
-	jQuery("#view-post-btn").after(sourceDiv);
-else
-	jQuery("#titlewrap").after(sourceDiv);
-
-//password field should be enabled as the password is automatically setin in oauth;
-jQuery("#pass2, #pass1").attr("noBanglaText","noBanglaText");
-
-//convert all text input field to accept bangla input
-//*
-if( window.location.href.indexOf("wp-admin/post-new.php") !== -1 || window.location.href.indexOf("wp-admin/profile.php") !== -1 )
-{
-	(function($){
-		try{
-			var $textbox= jQuery("input[type=text], textarea").not('[noBanglaText]');
-			$textbox.each(function(index,valueOfElement){
-				if(this.id == '')
-				{
-					this.id= Math.random();
-				}
-				new banglaLayout(this.id).loadHelpTooltip();
-			});	
-		}catch(e){}
-	})(jQuery);
-}
-//*/
-};
-
 // call jquery handling function
 if(!jQuery)
 {	
 	wp_enqueue_script('jQuery');       
-}	
-jQuery(handlerKeyboardPageOpener);
+}
 
+var handlerKeyboardPageOpener=function() {
+
+	var sourceDiv = "<span style='margin-left:5px;' id='rokeya_button_bn'><a onclick=\"window.open('http://seoul.freehostia.com', 'Rokeya', 'height=400,width=700,top=200,left=150,scrollbars=1');return false;\" class='button button-small'>অ.A</a></span>";
+	if( jQuery("#view-post-btn").length ==1 )
+		jQuery("#view-post-btn").after(sourceDiv);
+	else
+		jQuery("#titlewrap").after(sourceDiv);
+
+	//password field should be enabled as the password is automatically setin in oauth;
+	jQuery("#pass2, #pass1").attr("noBanglaText","noBanglaText");
+
+	//Load tooltip only in profile, post-new and "admin.php?page=XXX" pages.
+	if( window.location.href.indexOf("wp-admin/post-new.php") !== -1 || window.location.href.indexOf("wp-admin/profile.php") !== -1 || window.location.href.indexOf("wp-admin/admin.php?page=") !==-1 )
+	{
+		(function($){
+			try{
+				var $textbox= $("input[type=text], textarea").not('[noBanglaText]');
+				$textbox.each(function(index,valueOfElement){
+					if(this.id == '')
+					{
+						this.id= Math.random();
+					}
+					new banglaLayout(this.id).loadHelpTooltip();
+				});	
+			}catch(e){}
+		})(jQuery);
+	}
+//*/
+};
+	
+jQuery(handlerKeyboardPageOpener);
 </script>
 <?PHP
 }
