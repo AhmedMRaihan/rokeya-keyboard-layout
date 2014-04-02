a8a79b0c0ec13163091b4bb66926c2522f75b57a
mas1485@gmail.com <seoulgithub@users.noreply.github.com>
2014-04-02 20:43:34 +0000

    Font and related css upload.

--- rokeya_layout_wp.php
+++ rokeya_layout_wp.php
@@ -53,22 +53,6 @@ if(!jQuery)
 }	
 jQuery(handlerKeyboardPageOpener);
 
-(function($){
-	$.fn.loadHelpIconTooltip = function(){	
-
-	// this = collection of textarea or inputbox
-	this.each(function( index ) {
-		var toolTipText = "ctrl+m অথবা F9 চেপে বাংলা ও ইংরেজীতে সুইচ করতে পারবেন..\n..হ=H, ৎ=Z, ঙ=x, ঞ=X, ং=V, ঁ=B, ঃ=M";
-		var $comment=$(this);
-		var str="<div class='tooltipOnRokeyaLayout' style='width: 10px; position:relative; cursor:help; color:red;";
-		str +=	"left:"+($comment.width())+"px; top:17px'><abbr title='"+toolTipText+"'>?</abbr></div>";
-		$comment.before(str);
-		// write css
-	});
-	return this;
-	}
-
-})(jQuery);
 </script>
 <?PHP
 }
