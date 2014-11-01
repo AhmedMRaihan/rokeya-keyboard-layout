0c0c47252a84f62cde56db3608dbaff22c49cbd3
mas1485@gmail.com <seoulgithub@users.noreply.github.com>
2014-11-01 03:56:14 +0000

    Tinymce plugins are supported now

--- rokeya_layout-4.4.73.js
+++ rokeya_layout-4.4.73.js
@@ -391,10 +391,10 @@ Keyboard.prototype.writeFinalValue = function (finalText, caretPosition) {
 }
 
 Keyboard.prototype.tinymceplugin = function (text, caretPosition, e) {
-	if(text.charAt(caretPosition) == '<' && text.length == caretPosition+4)
+	if(text.charAt(caretPosition) == '<' && text.length == caretPosition+4 && (e.keyCode == 8 || e.keyCode == 46) )
 		return true; // A delete is pressed at the end and tinymce itself will take care of it
-	if(text.charAt(caretPosition) == '>' && caretPosition == 3)
-		return true; // Same above, but with backspace
+	if( (text == '' || (caretPosition >0 && text.charAt(caretPosition-1) == '>')) && (e.keyCode == 8 || e.keyCode == 46) )
+		return false; // Tinymce converts > to &lt; so it will never appear in text
 		
     this.text = text;
     this.caretPosition = caretPosition;
