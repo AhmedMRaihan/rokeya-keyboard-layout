22a26d68fb3a7126f47d0c61c6a5caaf92451ff4
MythicAngel <MythicAngel@users.noreply.github.com>
2016-08-15 18:43:22 +0600

    emulated real browser tests via jsdom in mochaJS

--- test/mochaTest.js
+++ test/mochaTest.js
@@ -1,4 +1,23 @@
-﻿var assert = chai.assert;
+﻿// window and document
+var jsdom = require('jsdom').jsdom;
+window = jsdom().defaultView;
+document = window.document;
+jQuery = require('jquery');
+$ = jQuery;
+navigator = window.navigator;
+
+// assertion
+chai = require('chai');
+assert = chai.assert;
+expect = chai.expect;
+
+// library
+var bnKeyboardSource = require("../rokeya_layout-4.4.73.js");
+var Keyboard = bnKeyboardSource.Keyboard;
+var banglaLayout = bnKeyboardSource.banglaLayout;
+
+// preparation (i.e. qunit-fixture)
+$("body").append('<textarea style="display:block" id="checkItOut" name="checkItOut"></textarea><input type="text" id="basicUsageEvents" />');
 
 describe('Installation', function () {
 
@@ -13,7 +32,7 @@ describe('Installation', function () {
         withEvents.beforeKeyEvent = function () { };
         withEvents.afterKeyEvent = function () { };
 
-        var bnLayoutWithEvents = new banglaLayout("eventInput", withEvents);
+        var bnLayoutWithEvents = new banglaLayout("basicUsageEvents", withEvents);
         assert.ok(bnLayoutWithEvents != null, "Class initiation failed");
 
         done();
@@ -23,6 +42,7 @@ describe('Installation', function () {
 describe('Keyboard Functionality', function () {
 
     it('Should handle vowel and consonants', function (done) {
+        
         var $textarea = $("#checkItOut");
         $textarea.val("");
         var pseudoKeyboard = new Keyboard();
@@ -44,7 +64,7 @@ describe('Keyboard Functionality', function () {
         });
 
         expectedString = 'আ';
-        msgOnError = "আ should be typed";
+        msgOnError = "আ should be inserted initially";
         $textarea.trigger(event);
 
         expectedString = 'আক';
@@ -52,17 +72,8 @@ describe('Keyboard Functionality', function () {
         event.keyCode = 75;
         event.which = 75;
         $textarea.trigger(event);
-        done();
-    });
 
-    it('should initiate for input[type=text]', function (done) {
-        var withEvents = {};
-        withEvents.beforeKeyEvent = function () { };
-        withEvents.afterKeyEvent = function () { };
-
-        var bnLayoutWithEvents = new banglaLayout("eventInput", withEvents);
-        assert.ok(bnLayoutWithEvents != null, "Class initiation failed");
-
+        assert.equal($textarea.val(), expectedString);
         done();
     });
 });
