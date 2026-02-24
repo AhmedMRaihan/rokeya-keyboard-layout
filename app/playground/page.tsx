638273ddf24f6c1f7bdb2ef6d566f2b53aecb8b3
ahmed.raihan <ahmed.raihan@klarna.com>
2026-02-24 12:07:03 +0100

    #1 Refactor to remove classes until required

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import React from "react";
+
 import PlaygroundComponent from "@/src/components/playground/mainComponent";
 
-export default class Playground extends React.Component{
-
-  render() {
-    return (
+export default function Playground() {
+  return (
       <PlaygroundComponent />
     );
-  }
 }
