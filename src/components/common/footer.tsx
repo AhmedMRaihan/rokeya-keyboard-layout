e3fa23b413ef5d390006940ccc416d7fb9330a89
User1 <user1@example.com>
2026-03-07 09:33:49 +0100

    #13 Rewrite footer and date display UI by replacing class components

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,17 +1,16 @@
 'use client';
 
-import React from "react";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const Footer: React.FunctionComponent = () => {
+const Footer = () => {
   const pathname = usePathname();
+  const isPlayground = pathname?.toLowerCase().endsWith("playground");
+
   return (
     <div className="footer_text text-center mt-8">
-      <Link
-        className="text-blue-600 dark:text-sky-400 p-4"
-        href="https://github.com/AhmedMRaihan/rokeya-keyboard-layout"
-      >
+      <Link className="text-blue-600 dark:text-sky-400 p-4"
+        href="https://github.com/AhmedMRaihan/rokeya-keyboard-layout">
         {"© " + new Date().getFullYear() + " Ahmed Raihan"}
       </Link>
       <Link className="text-blue-600 dark:text-sky-400 p-4" href="/home">
@@ -20,11 +19,11 @@ const Footer: React.FunctionComponent = () => {
       <Link className="text-blue-600 dark:text-sky-400 p-4" href="/coverage">
         ℹ️ Code Coverage
       </Link>
-      { ((pathname+"").toLowerCase().endsWith("playground") === false) && 
+      {!isPlayground && (
         <Link className="text-blue-600 dark:text-sky-400 p-4" href="/playground/">
           ✍️ Try the Playground
         </Link>
-      }
+      )}
     </div>
   );
 };
