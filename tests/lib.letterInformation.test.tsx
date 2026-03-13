200cce069edcc1ec8f88831f5be932945f7055b9
User1 <user1@example.com>
2026-03-13 21:19:35 +0100

    #13 Minor refactor for readability

--- tests/lib.letterInformation.test.tsx
+++ tests/lib.letterInformation.test.tsx
@@ -1,42 +1,46 @@
 import { describe, expect, test, beforeEach } from "vitest";
 import { LetterInformation } from "@/lib/LetterInformation";
 
-describe("LetterInformation", () => {
-  let letterInfo: LetterInformation;
-  const FOLLOWER_INDEX = 2; // Retrieves the 3rd follower in the sequence
+let letterInfo = new LetterInformation();
+const FOLLOWER_INDEX = 2; // Retrieves the 3rd follower in the sequence
 
-  beforeEach(() => {
-    letterInfo = new LetterInformation();
+describe("Consonants and Symbols Information", () => {
+  test("should return correct follower for a symbol (।)", () => {
+    // । → .
+    expect(letterInfo.getFollower("\u0964", FOLLOWER_INDEX)).toBe("\u002e"); 
   });
 
-  describe("Consonants and Symbols Information", () => {
-    test("should return correct follower for a symbol (।)", () => {
-      // । → .
-      expect(letterInfo.getFollower("\u0964", FOLLOWER_INDEX)).toBe("\u002e"); 
-    });
-
-    test("should return correct follower for a consonant (ব)", () => {
-      // ব → ভ
-      expect(letterInfo.getFollower("\u09ac", FOLLOWER_INDEX)).toBe("\u09ad"); 
-    });
-
-    test("should return empty string for a non-existent key", () => {
-      expect(letterInfo.getFollower("non-existent", FOLLOWER_INDEX)).toBe("");
-    });
+  test("should return correct follower for a consonant (ব)", () => {
+    // ব → ভ
+    expect(letterInfo.getFollower("\u09ac", FOLLOWER_INDEX)).toBe("\u09ad"); 
   });
 
-  describe("Vowels Information", () => {
-    test("should return correct follower for a vowel (া)", () => {
-      // া → আ
-      expect(letterInfo.getFollower("\u09be", FOLLOWER_INDEX)).toBe("\u0986"); 
-    });
-
-    test("should return correct follower for consecutive vowels ", () => {
-      expect(letterInfo.getConsecutiveVowel("\u09c1")).toBe("\u09c2");  
-    });
-
-    test("should return empty string for a non-existent key", () => {
-      expect(letterInfo.getConsecutiveVowel("non-existent")).toBe("");
-    });
+  test("should return empty string for a non-existent key", () => {
+    expect(letterInfo.getFollower("non-existent", FOLLOWER_INDEX)).toBe("");
   });
 });
+
+describe("Vowels Information", () => {
+  test("should return correct follower from kar-form to full-form", () => {
+    // া → আ
+    expect(letterInfo.getFollower("\u09be", FOLLOWER_INDEX)).toBe("\u0986"); 
+  });
+
+    // উ → ঊ
+  test("should return correct follower for consecutive vowels in full-form", () => {
+    expect(letterInfo.getConsecutiveVowel("\u09c1")).toBe("\u09c2");  
+  });
+});
+
+describe("Digits Information", () => {
+  test("should return correct follower for a number (১)", () => {
+    // ১ → ২
+    expect(letterInfo.getFollower("\u09e7", FOLLOWER_INDEX)).toBe("\u09e8"); 
+  });
+});
+
+describe("Non-existence check", () => {
+  test("should return empty string for a non-existent key", () => {
+    expect(letterInfo.getFollower("non-existent", FOLLOWER_INDEX)).toBe("");
+  });
+})
